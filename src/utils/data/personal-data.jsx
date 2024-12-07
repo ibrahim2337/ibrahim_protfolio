6746cd6b451314ac25451258507be92479c5a9f4
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-07 22:27:49 +0600

    refactor: update asset paths and improve project structure

--- src/utils/data/personal-data.jsx
+++ src/utils/data/personal-data.jsx
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Ibrahim Khalilullah",
-  profile: "/public/profile.png",
+  profile: "/profile.png",
   designation: "Software Developer",
   description:
     "I am Md Ibrahim Khalilullah, a professional and enthusiastic programmer with 1 years of experience in JavaScript, React.js, Next.js, Express.js and MongoDB. I am a quick learner with a self-learning attitude and a passion for problem-solving. I love to explore new technologies and am dedicated to making the web more accessible and open to the world. My core skill is based on JavaScript, and I enjoy using it to create high-quality, user-friendly web solutions. I've successfully completed numerous projects for clients worldwide, achieving 100% satisfaction. I am available for job opportunities that align with my skills and interests. Let's bring your web projects to life together!",
